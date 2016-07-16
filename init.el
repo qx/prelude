5190c75fef770236bf4592a1751bad1395eed252
qx <44708869@qq.com>
2016-07-16 17:57:12 +0800

    add custom

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;;; init.el --- Prelude's configuration entry point.
+;;; init.el --- Preludey's configuration entry point.
 ;;
 ;; Copyright (c) 2011-2016 Bozhidar Batsov
 ;;
@@ -32,6 +32,13 @@
 ;; Boston, MA 02110-1301, USA.
 
 ;;; Code:
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (defvar current-user
       (getenv
        (if (equal system-type 'windows-nt) "USERNAME" "USER")))
